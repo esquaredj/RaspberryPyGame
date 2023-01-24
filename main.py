2f0b55f1e70e95ef9006bd37c07b3c8d3a9ffaca
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2023-01-24 15:12:05 +0000

    basic game structure

--- main.py
+++ main.py
@@ -2,7 +2,24 @@ import sys
 import asyncio
 import pygame
 
-async def main():
-    print("hello world")
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Raspberry Pi Game")
+clock = pygame.time.Clock()
 
+surface = pygame.Surface((100, 200))
+surface.fill('deeppink')
+
+async def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.blit(surface, (0, 0))
+
+        await asyncio.sleep(0)
+        pygame.display.update()
+        clock.tick(30)
 asyncio.run(main())
