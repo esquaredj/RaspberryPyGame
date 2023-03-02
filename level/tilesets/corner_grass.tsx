efbec34cb9713aa11c9ca530ab0b076904249f32
shr-ev <95083865+shr-ev@users.noreply.github.com>
2023-03-02 22:01:00 +0000

    o.o

--- level/tilesets/corner_grass.tsx
+++ level/tilesets/corner_grass.tsx
@@ -2,9 +2,9 @@
 <tileset version="1.9" tiledversion="1.9.2" name="corner_grass" tilewidth="64" tileheight="64" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="64" height="64" source="../../../../../Users/Gaming/Downloads/grass_corner.png"/>
+  <image width="64" height="64" source="../../assets/Terrain/grass_corner.png"/>
  </tile>
  <tile id="1">
-  <image width="64" height="64" source="../../../../../Users/Gaming/Downloads/grass_corner_other.png"/>
+  <image width="64" height="64" source="../../assets/Terrain/grass_corner_other.png"/>
  </tile>
 </tileset>
