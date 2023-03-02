f9afac76e815537c6d6d2418371d377879b14b14
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2023-03-02 09:55:03 +0000

    dev

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import asyncio
 import pygame
+import sys
 from settings import *
 from level import Level
-from player import Player
 
 pygame.base.init()
 screen_width = 1200
@@ -11,15 +11,19 @@ screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Raspberry Pi Game")
 clock = pygame.time.Clock()
-level = Level(level_map, screen)
-player = Player((100, 100), screen, level.create_jump_particles)
+level = Level(None, screen)
 
 
 async def main():
     while True:
-        screen.fill('black')
-        level.run()
+        screen.fill('black')#
 
+        for event in pygame.event.get():
+            if event.type == pygame.constants.QUIT:
+                pygame.base.quit()
+                sys.exit()
+
+        level.run()
         await asyncio.sleep(0)
         pygame.display.update()
         clock.tick(30)
--- settings.py
+++ settings.py
@@ -1,16 +1,5 @@
-level_map = [
-    '                                    ',
-    '                                    ',
-    '                            XXXXXXXX',
-    ' X    XXXX            XX      X XXXX',
-    ' XX           P                XXX  ',
-    ' XXXX         XX         XX X  XXXXX',
-    ' XXXX       XX              X    XXX',
-    ' XX    X  XXXX    XX  XX    XX   XXX',
-    '       X  XXXX    XX  XXX   XX X XXX',
-    '    XXXX  XXXXXX  XX  XXXX  XXXX XXX',
-    'XXXXXXXX  XXXXXX  XX  XXXX  XXXX XXX']
-
+vertical_tiles = 20
 tile_size = 64
-screen_width = 1200
-screen_height = len(level_map) * tile_size
+
+screen_height = vertical_tiles * tile_size
+screen_width = screen_height * 16 // 9
